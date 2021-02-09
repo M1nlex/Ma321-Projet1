7047da061d70173e50d55cbdb68a132a283e4cb4
Rabonarca <romaric.dubois17062000@gmail.com>
2021-02-09 08:44:15 +0100

    add fuction

--- functions.py
+++ functions.py
@@ -2,15 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-p = open("dataP.dat", "r")
-q = open("dataQ.dat", "r")
-a = np.zeros(50)
-b = np.zeros(50)
-for i in range(50):
-    a[i] = p.readline()
-    b[i] = q.readline()
+def open_data():
+    p = open("dataP.dat", "r")
+    q = open("dataQ.dat", "r")
+    a = np.zeros(50)
+    b = np.zeros(50)
+    for i in range(50):
+        a[i] = p.readline()
+        b[i] = q.readline()
 
-print(a, b)
 
-plt.plot(a,b, "x")
-plt.show()
+
+if __name__ == '__main__':
+    print(a, b)
+    plt.plot(a,b, "x")
+    plt.show()
