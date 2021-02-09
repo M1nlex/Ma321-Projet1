a4b7b0027e9022d38329f4bc9da8b29b4adb6afd
Rabonarca <romaric.dubois17062000@gmail.com>
2021-02-09 09:42:06 +0100

    bugfixes

--- functions.py
+++ functions.py
@@ -2,6 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from regression_lineaire import *
 
+
+def open_data(file):
+    p = open(file, "r")
+    a = []
+    for line in p:
+        a.append(float(line))
+
+    return a
+
+
 # Ajustement linéaire
 
 
