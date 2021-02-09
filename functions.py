dffc47a6e02d78bc3a5072491d2d3fe7103a114e
Marabricot <m.blin@hotmail.com>
2021-02-09 09:58:11 +0100

    oui

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ from regression_lineaire import *
 # Ajustement linéaire
 
 # Tracer le nuage de points
+# lecture des données 1
 def open_data(file):
     p = open(file, "r")
     a = []
@@ -12,9 +13,11 @@ def open_data(file):
         a.append(float(line))
     return a
 
+# Lecture des données 2
 p=np.loadtxt('dataP.dat')
 q=np.loadtxt('dataQ.dat')
 
 plt.scatter(p,q)
 
+# Approx par une fonction
 ComparaisonPolynomeRegression(p,q,1)
