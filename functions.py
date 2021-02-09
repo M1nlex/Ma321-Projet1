b3c2ba030a22bc3913b512440b69591b79b7e298
M1nlex <aleandrepe@gmail.com>
2021-02-09 09:52:12 +0100

    debug function

--- functions.py
+++ functions.py
@@ -2,29 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from regression_lineaire import *
 
-
-def open_data(file):
-    p = open(file, "r")
-    a = []
-    for line in p:
-        a.append(float(line))
-
-    return a
-
-
 # Ajustement linéaire
 
-# Lecture des données V1
+# Tracer le nuage de points
 def open_data(file, length):
+    p = open(file, "r")
     a = np.zeros(length)
     for i in range(length):
         a[i] = p.readline()
+    return a
 
-# Lecture des données V2 et tracé du nuage de points
-p = np.loadtxt('dataP.dat')
-q = np.loadtxt('dataQ.dat')
+p=np.loadtxt('dataP.dat')
+q=np.loadtxt('dataQ.dat')
+
 plt.scatter(p,q)
-#plt.show()
 
-# Approximation par une fonction
 ComparaisonPolynomeRegression(p,q,1)
