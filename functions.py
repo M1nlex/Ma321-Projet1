e01025f8b64695c46dc057e24fdea2cee6ae1006
Rabonarca <romaric.dubois17062000@gmail.com>
2021-02-09 08:38:55 +0100

    add file opening

---
+++ functions.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+p = open("dataP.dat", "r")
+q = open("dataQ.dat", "r")
+a = np.zeros(50)
+b = np.zeros(50)
+for i in range(50):
+    a[i] = p.readline()
+    b[i] = q.readline()
+
+print(a, b)
+
+plt.plot(a,b, "x")
+plt.show()
