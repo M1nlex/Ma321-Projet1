5d9e096add26b9b2b9b86bf6e685d52266e5dd5b
Marabricot <m.blin@hotmail.com>
2021-02-09 09:42:31 +0100

    Regression ne fonctionne pas je crois

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ from regression_lineaire import *
 
 # Ajustement linéaire
 
-# Tracer le nuage de points
+# Lecture des données V1
 def open_data(file, length):
     p = open(file, "r")
     a = np.zeros(length)
@@ -12,11 +12,11 @@ def open_data(file, length):
         a[i] = p.readline()
     return a
 
-p=np.loadtxt('dataP.dat')
-q=np.loadtxt('dataQ.dat')
-
+# Lecture des données V2 et tracé du nuage de points
+p = np.loadtxt('dataP.dat')
+q = np.loadtxt('dataQ.dat')
 plt.scatter(p,q)
+#plt.show()
 
+# Approximation par une fonction
 ComparaisonPolynomeRegression(p,q,1)
-
-
