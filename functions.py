33fae549b3219144e67571d7b985399ca14fa2b7
Rabonarca <romaric.dubois17062000@gmail.com>
2021-02-09 08:52:14 +0100

    bugfixes

--- functions.py
+++ functions.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def open_data(file):
+def open_data(file, length):
     p = open(file, "r")
-    a = np.zeros(50)
-    for i in range(50):
+    a = np.zeros(length)
+    for i in range(length):
         a[i] = p.readline()
 
     return a
