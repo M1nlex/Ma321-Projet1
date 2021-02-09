2de3197b7e4a5814163e08103634ac592965b5d4
Rabonarca <romaric.dubois17062000@gmail.com>
2021-02-09 08:47:36 +0100

    bugfixes

--- functions.py
+++ functions.py
@@ -2,18 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def open_data():
-    p = open("dataP.dat", "r")
-    q = open("dataQ.dat", "r")
+def open_data(file):
+    p = open(file, "r")
     a = np.zeros(50)
-    b = np.zeros(50)
     for i in range(50):
         a[i] = p.readline()
-        b[i] = q.readline()
 
+    return a
 
 
 if __name__ == '__main__':
-    print(a, b)
-    plt.plot(a,b, "x")
+    p = open_data("dataP.dat")
+    q = open_data("dataQ.dat")
+
+    print(p, q)
+    plt.plot(p, q, "x")
     plt.show()
