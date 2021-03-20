b39b8d2adf71cf99eb590d97540af80bc099ef6f
rabonarca <romaric.dubois17062000@gmail.com>
2021-03-20 22:15:16 +0100

    Add answer to question 2.2.1.5 a (nice 3d curve btw)

--- part_3_4_1.py
+++ part_3_4_1.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from Projet1 import *
 from mpl_toolkits import mplot3d
 
-met = 1
+met = 0
 pas = 0.01
 #3.4.1.a
 x = np.arange(-10,10.000001,pas)
@@ -22,9 +22,20 @@ w=X.T@q
 
 F = 0.5 * ( Z[0][0]*(c1**2) + 2*Z[0][1]*c1*c2 + Z[1][1]*(c2**2) - 2*(w[0]*c1 + w[1]*c2) + s )
 
+
+
+
 if met == 0:
     print(c1)
     print(c2)
 if met ==1: # méthode 1
     res = plt.contour(c1,c2,F,1000)
     plt.show()
+
+
+
+# 2.2.1.5 a
+
+ax3d = plt.axes(projection='3d')
+ax3d.plot_surface(c1, c2, F)
+plt.show()
