fcd5134aaf9cf9d69d3c0d130c9bfbef7db83329
AliElKhalil <ali.el-khalil@ipsa.fr>
2021-03-24 08:03:06 +0100

    Update Code qui ne marche pas sa mère (sur les fonctions partielles).py

--- Code qui ne marche pas sa mère (sur les fonctions partielles).py
+++ Code qui ne marche pas sa mère (sur les fonctions partielles).py
@@ -17,24 +17,44 @@ q=np.loadtxt('dataQ.dat')
 A=Det_A(p)
 b=Det_b(p,q)
 
-def minimum_de_F():         #fonction qui retourne le minimum de F pour l'utiliser dans le traçage des fonctions partielles au point critique suivant un vecteur
+def minimum_de_F():         
     x=np.linalg.solve(A,b)
-    return 0.5*np.dot(np.transpose([x]), np.dot(A,x))-np.dot(np.transpose([b]), x)+0.5*np.linalg.norm(q)**2
+    k=0.5*np.dot(x, np.dot(A,np.transpose([x])))
+    l=np.dot(b, np.transpose([x]))
+    m=0.5*np.linalg.norm(q)**2
+    return k-l+m
 
 
-def fonction_partielles(): #traçage des courbes des fonctions partielles au point critiques et selon les vecteurs de la base canonique(e1 et e2) et les vecteurs propres de A (v1 et v2)
-    e1=[[0],[1]]
-    e2=[[1],[0]]
-    v1,v2=val_vect_propres()[1] #fonction dans le code d'Elena
+def fonction_partielles(): 
+    e1=np.array([[0],[1]])
+    e2=np.array([[1],[0]])
+    v1,v2=val_vect_propres()[1] 
     v1=np.transpose([v1])
     v2=np.transpose([v2])
     I=np.linspace(-10,10,10000)
     D=[e1,e2,v1,v2]
+    i=1
     for d in D:
         K=[]
         for t in I:
-            F=0.5*np.dot(np.transpose(d),np.dot(A,d))*t*t+minimum_de_F()
+            k=0.5*np.dot(np.transpose(d),np.dot(A,d))*t*t
+            m=minimum_de_F()[0]
+            F=(k+m)[0]
             K.append(F)
-    plt.plot(a,K)
-    plt.show()
+        plt.plot(I,K)
+        plt.xlabel("t")
+        plt.ylabel("F(c*+td)")
+        plt.title("Courbe de la fonction partielle en c* suivant"+str(d))
+        plt.show()
 
+
+def pointcritique():
+    m=len(p)
+    k=np.dot(p, np.ones(m))
+    l=m*np.linalg.norm(p)**2-k**2
+    K=((1/m)+k/(l*m))*np.dot(q,np.ones(m))
+    L=(-k**2/(m*l))*np.dot(q,np.ones(m))+(k/l)*np.dot(p,np.transpose([q]))
+    c=np.array([K,L[0]])
+    return c
+
+print (pointcritique())    
