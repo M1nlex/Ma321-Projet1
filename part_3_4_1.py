c977d67f91e4f286898fae365f7b1863cf8e757c
rabonarca <romaric.dubois17062000@gmail.com>
2021-03-26 10:42:17 +0100

    fix q_2_2_1_5

--- part_3_4_1.py
+++ part_3_4_1.py
@@ -111,6 +111,18 @@ def q_3_4_2():
 # 2.2.1.5 a
 # --------------------------------------
 def q_2_2_1_5():
+    pas = 0.01
+    x = np.arange(-10, 10.000001, pas)
+    y = np.arange(-10, 10.000001, pas)
+    c1, c2 = np.meshgrid(x, y)
+
+    p, q = recup_donnees()
+    X = Creation_de_X(p)
+    Z = X.T @ X
+    s = q.T @ q
+    w = X.T @ q
+
+    F = 0.5 * (Z[0][0] * (c1 ** 2) + 2 * Z[0][1] * c1 * c2 + Z[1][1] * (c2 ** 2) - 2 * (w[0] * c1 + w[1] * c2) + s)
     ax3d = plt.axes(projection='3d')
     ax3d.plot_surface(c1, c2, F)
     plt.show()
